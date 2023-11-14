2d6df6dfe36b888a35ec97d1c2fabeb9521ff70c
arif <muhdarifikmal99@gmail.com>
2023-11-14 23:36:22 +0800

    install requirements

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'object_detection',
+    version= '0.0.0',
+    author= 'Arif Ikmal',
+    author_email= 'muhdarifikmal99@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
