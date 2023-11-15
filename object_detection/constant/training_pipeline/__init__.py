63f193a994cdf112e49a675c6e29861685212b0c
arif <muhdarifikmal99@gmail.com>
2023-11-15 11:22:13 +0800

    validation added

--- object_detection/constant/training_pipeline/__init__.py
+++ object_detection/constant/training_pipeline/__init__.py
@@ -7,3 +7,14 @@ Data Ingestion related constant start with DATA_INGESTION VAR NAME
 DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_DOWNLOAD_URL: str = "https://drive.google.com/file/d/1pvG0byrcSu3NWO7Uf6z9gYSnJ4gWcB0P/view?usp=sharing"
+
+
+"""
+Data Validation realted constant start with DATA_VALIDATION VAR NAME
+"""
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+
+DATA_VALIDATION_STATUS_FILE = 'status.txt'
+
+DATA_VALIDATION_ALL_REQUIRED_FILES = ["train", "valid", "data.yaml"]
