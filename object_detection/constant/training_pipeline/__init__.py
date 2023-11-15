4eeb3c8b1c0e17cf8e37f69a39cf23e9463b1ba7
arif <muhdarifikmal99@gmail.com>
2023-11-15 12:33:19 +0800

    latest

--- object_detection/constant/training_pipeline/__init__.py
+++ object_detection/constant/training_pipeline/__init__.py
@@ -6,7 +6,7 @@ Data Ingestion related constant start with DATA_INGESTION VAR NAME
 
 DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
-DATA_DOWNLOAD_URL: str = "https://drive.google.com/file/d/1pvG0byrcSu3NWO7Uf6z9gYSnJ4gWcB0P/view?usp=sharing"
+DATA_DOWNLOAD_URL: str = "https://drive.google.com/file/d/1ho2SQtYuiA9r8vvgcmc2EHzNk56y5klP/view?usp=sharing"
 
 
 """
